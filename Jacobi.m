223d119f22cfa18899fd786bdf6083b611b77e16
brandonkuhnert <brandonkuhnert@berkeley.edu>
2016-03-27 22:53:20 -0700

    Added files via upload

--- Jacobi.m
+++ Jacobi.m
@@ -1,17 +1,27 @@
-function [ x ] = Jacobi( A,b,k,x )
+function [ x,rho ] = Jacobi( A,b )
 %Input: a matrix, the b vector, and number of iterations k, 
 %value for x^(0)
 %output: the x vector
 
-if x==0;
-    x=zeros(length(A),1);
-    D=diag(diag(A));
-    F=inv(D);
-    
-    for i=1:k;
-        x=(D*F-F*A)*x+F*b;
-    end
+% diagonal part of A and rest
+D = diag(diag(A));
+R = A - D;
+
+% iteration matrix and offset
+T = - inv(D) * R;
+C = inv(D) * b;
+
+% spectral radius condition
+rho = max(abs(eig(T)));
+if rho >= 1
+    error('no convergence')
 end
 
+% initial guess
+x = randn(size(b));
+
+% iteration
+while norm(A * x - b) > 1e-15
+    x = T * x + C;
 end
 
